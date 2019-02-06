7a03a0fdbac2ca52abb54813f4da9bd86f9b0e75
Sean <Spglancy@gmail.com>
2019-02-06 11:02:36 -0800

    timo testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
  */
 router.get('/signup', (req, res) => {
     const x = req.nToken;
-    if(x){
+    if (x) {
         res.redirect(`/home/${x._id}`)
     }
     res.render('signup');
@@ -22,7 +22,7 @@ router.get('/signup', (req, res) => {
  */
 
 router.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 })
 
 /**
@@ -30,49 +30,50 @@ router.get('/login', (req, res) => {
  */
 
 router.post('/register', (req, res) => {
-  var user = new User(req.body);
-  user.save().then((user) => {
-      var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
-      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-      res.redirect(`/home/${user._id}`);
-  });
-  });
+    var user = new User(req.body);
+    user.save().then((user) => {
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+        // res.redirect(`/home/${user._id}`);
+        res.send(req.body)
+    });
+});
 
-  // clears cookie
+// clears cookie
 router.get('/logout', (req, res) => {
-      res.clearCookie('nToken');
-      res.redirect('/');
-    });
+    res.clearCookie('nToken');
+    res.redirect('/');
+});
 
-    // checks user auth and logs in
+// checks user auth and logs in
 router.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  // Find this user name
-  User.findOne({ email }, "email password")
-    .then(user => {
-      if (!user) {
-        // User not found
-        return res.status(401).send({ message: "Wrong Email or Password" });
-      }
-      // Check the password
-      user.comparePassword(password, (err, isMatch) => {
-        if (!isMatch) {
-          // Password does not match
-          return res.status(401).send({ message: "Wrong Email or Password" });
-        }
-        // Create a token
-        const token = jwt.sign({ _id: user._id, email: user.email, name: user.name }, process.env.SECRET, {
-          expiresIn: "60 days"
+    const email = req.body.email;
+    const password = req.body.password;
+    // Find this user name
+    User.findOne({ email }, "email password")
+        .then(user => {
+            if (!user) {
+                // User not found
+                return res.status(401).send({ message: "Wrong Email or Password" });
+            }
+            // Check the password
+            user.comparePassword(password, (err, isMatch) => {
+                if (!isMatch) {
+                    // Password does not match
+                    return res.status(401).send({ message: "Wrong Email or Password" });
+                }
+                // Create a token
+                const token = jwt.sign({ _id: user._id, email: user.email, name: user.name }, process.env.SECRET, {
+                    expiresIn: "60 days"
+                });
+                // Set a cookie and redirect to root
+                res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                res.redirect(`/home/${user._id}`);
+            });
+        })
+        .catch(err => {
+            console.log(err);
         });
-        // Set a cookie and redirect to root
-        res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
-        res.redirect(`/home/${user._id}`);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
 });
 
 module.exports = router;
