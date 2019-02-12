4c1646f089ecc76e70ef4cce54ecaa4ada503c1b
Sean <Spglancy@gmail.com>
2019-02-12 15:54:19 -0800

    added new data points to model

--- models/data.js
+++ models/data.js
@@ -3,6 +3,16 @@ const Schema = mongoose.Schema;
 
 const dataSchema = new Schema({
     heartRateData: Object,
+    heightData: Object,
+    bloodPressureSystolicData: Object,
+    bloodPressureDiastolicData: Object,
+    bodyMassData: Object,
+    bodyTemperatureData: Object,
+    activeEnergyBurnedData: Object,
+    leanBodyMassData: Object,
+    respiratoryRateData: Object,
+    restingHeartRateData: Object,
+    stepCountData: Object
 });
 
 module.exports = mongoose.model("Data", dataSchema);
