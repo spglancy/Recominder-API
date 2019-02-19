eb6c2d59072f71c362f6e581e2e97d455604e31b
Sean <Spglancy@gmail.com>
2019-02-19 11:52:36 -0800

    added paginiation hack

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const app = express()
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const config = require('./config')
-const expressValidator = require('express-validator')
-const jwt = require('jsonwebtoken');
 const authController = require('./controllers/authController')
 var cookieParser = require('cookie-parser');
 
-app.use(bodyParser.json())
+app.use(express.bodyParser({limit: '50mb'}))
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(expressValidator())
-app.use(methodOverride('_method'))
 app.use(cookieParser());
 
 mongoose.connect(config.mongoURL, { useNewUrlParser: true })
