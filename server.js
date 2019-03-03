349cf2920fb8c7e0b80ee2ab16d213b5737aab27
Sean <Spglancy@gmail.com>
2019-03-02 17:46:00 -0800

    updated body-parser allowed size

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ let cookieParser = require('cookie-parser')
 
 // cors protocols? fix I dont understand fully
 app.use(cors())
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({ limit: '200mb' }))
+app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }))
 app.use(cookieParser())
 
 mongoose.connect(config.mongoURL, { useNewUrlParser: true })
